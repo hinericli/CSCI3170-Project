72095bb7a2c68f690f185894f697a71681559e93
hinericli <83932381+hinericli@users.noreply.github.com>
2024-12-21 19:55:07 +0800

    listManufacturer v1

--- src/hi/Manager.java
+++ src/hi/Manager.java
@@ -101,7 +101,59 @@ public class Manager {
 		}
 	}
 	
-	private static void listManufacturers(Connection conn) { } 
+	private static void listManufacturers(Connection conn) { 
+		try {
+			Statement stmt = conn.createStatement();
+			ResultSet rs;
+			
+			/*
+			String query = "WITH PTABLE AS (SELECT PART.mID, PART.pID, PART.pPrice, PART.pPrice*COUNT(PART.pID) AS pTotalSales "
+					+ "FROM TRANSACTION JOIN PART ON PART.pID=TRANSACTION.pID "
+					+ "GROUP BY PART.mID, PART.pID, PART.pPrice "
+					+ "ORDER BY PART.pID ASC) "
+					
+					+ "SELECT MANUFACTURER.mID, MANUFACTURER.mName, PTABLE.pID, SUM(PTABLE.pTotalSales) AS pSum "
+					+ "FROM MANUFACTURER JOIN PTABLE ON MANUFACTURER.mID=PTABLE.mID "
+					+ "GROUP BY MANUFACTURER.mID, MANUFACTURER.mName, PTABLE.pID, PTABLE.pPrice, PTABLE.pTotalSales "
+					+ "ORDER BY PTABLE.pTotalSales DESC";
+			rs = stmt.executeQuery(query);*/
+		    
+			
+		    String pTableQuery = "SELECT PART.mID, PART.pID, PART.pPrice, PART.pPrice*COUNT(PART.pID) AS pTotalSales "
+					+ "FROM TRANSACTION JOIN PART ON PART.pID=TRANSACTION.pID "
+					+ "GROUP BY PART.mID, PART.pID, PART.pPrice "
+					+ "ORDER BY PART.pID ASC";
+		    rs = stmt.executeQuery(pTableQuery);
+		    System.out.println("| mID | pID | pPrice | pTotalSales");
+		    while (rs.next()) {
+		    	System.out.print("| " + rs.getString("mID"));
+		        System.out.print(" | " + rs.getString("pID"));
+		        System.out.print(" | " + rs.getString("pPrice"));
+		        System.out.print(" | " + rs.getString("pTotalSales"));
+		        System.out.println(" |");
+		    }
+			
+		    /*
+			System.out.println("| Manufacturer ID | Manufacturer Name | Total Sales Value |");
+		    while (rs.next()) {
+		        System.out.print("| " + rs.getString("mID"));
+		        System.out.print(" | " + rs.getString("mName"));
+
+		        System.out.print(" | " + rs.getString("pID"));
+		        System.out.print(" | " + rs.getString("pPrice"));
+		        System.out.print(" | " + rs.getString("pTotalSales"));
+		        System.out.print(" | " + rs.getString("pSum"));
+		        
+		        System.out.println(" |");
+		    }*/
+		    
+		    
+		} catch(Exception e) {
+			System.err.println(e);
+		}
+		
+		
+	} 
 	
 	private static void showNMostPopularParts(Connection conn, int n) { }
 
